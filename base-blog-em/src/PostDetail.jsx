02fcf5d083eee16f78b218b8b2a61567bbee19d4
wookki <bwlimtony@gmail.com>
2023-09-24 22:22:41 +0900

    chore: infinite scroll 초기설정

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,6 +1,8 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
   );
   return response.json();
 }
@@ -8,7 +10,7 @@ async function fetchComments(postId) {
 async function deletePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
+    { method: "DELETE" },
   );
   return response.json();
 }
@@ -16,22 +18,37 @@ async function deletePost(postId) {
 async function updatePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } },
   );
   return response.json();
 }
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id),
+  );
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+
+  const updateMutation = useMutation((postId) => updatePost(postId));
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      <button
+        onClick={() => {
+          updateMutation.mutate(post.id);
+        }}
+      >
+        Update title
+      </button>
       <p>{post.body}</p>
+      {updateMutation.isSuccess && <h2>안녕하세요</h2>}
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data?.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
